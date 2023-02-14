07c6bf7674d6d3497077f91db65f8da4fd31f31c
AlbaSimas <gigismile.simas@gmail.com>
2023-02-14 09:44:29 -0500

    Portifolio Mentoria Paulo

---
+++ app.js
@@ -0,0 +1,56 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const nodemailer = require("nodemailer");
+const cors = require('cors');
+
+app.use(cors({
+    origin: '*'
+}));
+
+app.use(express.json());
+
+
+app.post("/send-email", cors(), async (req, res) => {
+    const data = req.body
+    const email = data?.email;
+    const subject = data?.subject;
+    const text = data?.text;
+    const html = data?.html ? data?.html : null;
+    if (email === undefined || subject === undefined || text === undefined)
+    res.send("Dados inválidos!!!");
+    console.log(process.env.USUARIO, process.env.PASSWORD);
+
+    const transport = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+          user: process.env.USUARIO,
+          pass: process.env.PASSWORD,
+        },
+      });;
+
+    const mailOptions = {
+        from: email,
+        to:  process.env.USUARIO,
+        subject: subject,
+        text: text,
+        html: html ? html : null,
+    };
+    await transport.sendMail(mailOptions)
+    .then(() => {
+        console.log("Sucesso")
+    })
+    .catch(erro => {
+        console.log(erro)
+    });
+
+    return res.json({
+        erro: false,
+        mensagem: "Email enviado com sucesso"
+
+    });
+});
+
+app.listen(process.env.PORT || 3000); 
